e8e594b528e9e134c9946072d5be04bfc256fa25
Peter Bourgelais <peter.bourgelais@gmail.com>
2018-08-20 21:27:40 -0400

    Flipped bCanEverTick in Tank.cpp

--- BattleTank/Source/BattleTank/Tank.h
+++ BattleTank/Source/BattleTank/Tank.h
@@ -25,8 +25,6 @@ protected:
     UTankAimingComponent* aimingComponent = nullptr;
     
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
--- BattleTank/Source/BattleTank/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/TankBarrel.cpp
@@ -2,9 +2,9 @@
 
 #include "TankBarrel.h"
 
-void UTankBarrel::Elevate(float elevateSpeed)
+void UTankBarrel::Elevate(float relativeSpeed)
 {
     //Rotate (lerp in Unity-speak) to the new rotation ONLY along the pitch given a max rotation speed and the frame time
     //(wanna keep this frame independent)
-    UE_LOG(LogTemp, Warning, TEXT("In tankBarrel->Elevate() with speed %f"), elevateSpeed)
+    UE_LOG(LogTemp, Warning, TEXT("In tankBarrel->Elevate() with speed %f"), relativeSpeed)
 }
